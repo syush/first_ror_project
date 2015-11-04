971ef2dbe2c05f780f563f09a0bb6f3e40dcfccc
Sergey Shishlov <shishlov@gmail.com>
2015-11-05 00:02:16 +0300

    validation added; json removed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,11 @@ class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
   # GET /comments
-  # GET /comments.json
   def index
     @comments = Comment.all
   end
 
   # GET /comments/1
-  # GET /comments/1.json
   def show
   end
 
@@ -22,7 +20,6 @@ class CommentsController < ApplicationController
   end
 
   # POST /comments
-  # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
 
@@ -36,7 +33,6 @@ class CommentsController < ApplicationController
   end
 
   # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
   def update
     respond_to do |format|
       if @comment.update(comment_params)
@@ -48,7 +44,6 @@ class CommentsController < ApplicationController
   end
 
   # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
     @comment.destroy
     respond_to do |format|
