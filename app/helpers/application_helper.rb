3765061a25d6b287988c9d2e0b26a7ea7099ec04
Sergey Shishlov <shishlov@gmail.com>
2015-11-27 05:22:34 +0300

    add icons to links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def icon_link_to(icon, path, options = {})
+    title = options.delete(:title)
+    link_to path, options do
+      content_tag(:i, " "+title, class: "fa fa-#{icon} links")
+    end
+  end
+
+  def edit_link(path, options={})
+    options[:title] = t(:edit)
+    icon_link_to('pencil-square-o', path, options)
+  end
+
+  def delete_link(path, options={})
+    options[:title] = t(:delete)
+    icon_link_to('trash-o', path, options)
+  end
 end
